5b41e6050ad52452a6120da372f5af5291928804
julietaifran44-cpu <julietaifran44@gmail.com>
2025-08-21 12:00:40 -0300

    págV1

---
+++ script.js
@@ -0,0 +1,17 @@
+function openNav() {
+    document.getElementById("navHamg").style.width = "75%";
+
+    const botonAbrir = document.getElementById('botonNavAbrirImg');
+    botonAbrir.style.visibility = 'hidden';
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("navHamg").style.width = "0%";
+    
+    setTimeout(() => {
+        const botonAbrir = document.getElementById('botonNavAbrirImg');
+    botonAbrir.style.visibility = 'visible';
+      }, "200");
+        
+}
